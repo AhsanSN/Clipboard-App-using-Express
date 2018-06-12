f86a62811f718c78dac3b4fbe9c18afbaac2a7fe
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-12 13:53:51 +0500

    added css

--- router.js
+++ router.js
@@ -13,27 +13,26 @@ function home()
     app.use(express.static('public')); //inline
 
     //displaying html requested page
-    console.log("h2")
     app.get('/', function (req, res) {
       //res.send('Hello World!');
       //res.render('index');
-      console.log("h1")
       res.render('index', {Name: null, error: null});
-    })
-    //displaying form response
-    app.post('/', function (req, res) {
+
+      app.post('/', function (req, res) {
       //res.render('index');
       var cityName = req.body.city;
       console.log(req.body.city);
 
       //sending data to index file with 
       if(cityName != ''){
+        //add to database
         res.render('index', {Name: cityName, error: null});
-      }else
-      {
-        res.render('index', {Name: null, error: null});
       }
     })
+
+    })
+    //displaying form response
+    
 
     app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
