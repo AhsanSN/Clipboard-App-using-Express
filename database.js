b6dfc57f52e74fe6bd7884abb44ad86f05d7fc00
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-06-13 18:22:08 +0500

    noticeboard added

--- database.js
+++ database.js
@@ -1,38 +1,54 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql');
+const sys = require('sys');
+var messageArrayTemp = [];
 
-    var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'sampleDB'
-    });
 
-    connection.connect(function(error){
-    if(error){
-        console.log("error");
-    }
-    else{
-        console.log("connection successfull");
-    }
-    });
+var connection = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'sampleDB'
+});
+
+connection.connect(function(error){
+if(error){
+    console.log("error");
+}
+else{
+    console.log("connection successfull");
+}
+});
 
 
 function addToDb(message)
 {
-    app.get('/',function (req, resp){
-    connection.query("SELECT * FROM sampletable", function(error, rows, fields){
+    connection.query(`INSERT INTO sampletable (name) VALUE ('${message}')`, function(error, rows, fields){
         if(error){
         console.log("error in query");
     }
-    else{
-        console.log("query successfull");
-        console.log(rows[0].name);
-        resp.send("hello"+rows[0].name);    
-    }
     }); 
+}
+
+function selectFromDb(callback)
+{
+    connection.query(`SELECT name FROM sampletable`, function(error, rows, fields){
+        if(error){
+        console.log("error in query");
+    }
+    var lengthRows = rows.length;
+    for (var i = lengthRows - 1; i >= 0; i--) {
+    	messageArrayTemp.push(rows[i].name);
+    }
+    console.log(messageArrayTemp.length);
+    console.log("ret 1 "+messageArrayTemp);
+    cb(messageArrayTemp);
+
     });
+    console.log("ret 2 "+messageArrayTemp);
+    return messageArrayTemp;
 }
 
 module.exports.addToDb = addToDb;
+module.exports.selectFromDb = selectFromDb;
--- router.js
+++ router.js
@@ -5,72 +5,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 const database = require('./database');
 
-
-// //db connection
-//const mysql = require('mysql');
-// function establishConnection()
-// {
-//     var connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'sampleDB'
-//     });
-
-//     connection.connect(function(error){
-//     if(error){
-//         console.log("error");
-//     }
-//     else{
-//         console.log("connection successfull");
-//     }
-//     });
-// }
-
-// function addToDb(message)
-// {
-//     establishConnection();
-//     app.get('/',function (req, resp){
-//     connection.query("SELECT * FROM sampletable", function(error, rows, fields){
-//         if(error){
-//         console.log("error in query");
-//     }
-//     else{
-//         console.log("query successfull");
-//         console.log(rows[0].name);
-//         resp.send("hello"+rows[0].name);    
-//     }
-//     }); 
-//     });
-// }
 //for html
 function home()
 {
     //for css
     app.use(express.static('public')); //inline
-
     //displaying html requested page
     app.get('/', function (req, res) {
-      //res.send('Hello World!');
-      //res.render('index');
-      res.render('index', {Name: null, error: null});
+        res.render('index', {Name: null, error: null});
     })
+
+    app.get('/noticeboard', function (req, res) {
+      
+      var messageArray = database.selectFromDb();
+      console.log("Array "+ messageArray);
+      res.render('noticeboard', {messages: messageArray, error: null});
+    });
 
     //displaying form response
     app.post('/', function (req, res) {
-      //res.render('index');
-      var cityName = req.body.city;
+      var message = req.body.city;
       console.log(req.body.city);
 
       //sending data to index file with 
-      if(cityName != ''){
+      if(message != ''){
         //add to database
-        database.addToDb(cityName);
-        res.render('index', {Name: cityName, error: null});
+        database.addToDb(message);
+        res.render('index', {Name: message, error: null});
       }
     })
     
-
     app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
